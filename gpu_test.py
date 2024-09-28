149321f0a1184c0d0fade0373f68b3b9cd159d7a
LeeSunMyeong <lsmtjsaud@naver.com>
2024-09-28 15:54:23 +0900

    FEAT pneumonia classification model - ERROR in loading model

--- gpu_test.py
+++ gpu_test.py
@@ -1,9 +1,201 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pydicom import dcmread
+from PIL import Image
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
 import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils import data
+
+import torch.nn.functional as F
+from torchvision.utils import make_grid, save_image
+
+from matplotlib import rcParams
+
+import matplotlib.patches as patches
+from math import ceil
+
+label_data = pd.read_csv('C:/Users/smmm/Downloads/rsna-pneumonia-detection-challenge/stage_2_train_labels.csv')
+columns = ['patientId', 'Target']
+all_data = label_data
+
+label_data = label_data.filter(columns)
+train_labels, val_labels = train_test_split(label_data.values, test_size=0.1)
+
+train_f = 'C:/Users/smmm/Downloads/rsna-pneumonia-detection-challenge/stage_2_train_images'
+test_f = 'C:/Users/smmm/Downloads/rsna-pneumonia-detection-challenge/stage_2_test_images'
+
+train_paths = [os.path.join(train_f, image[0]).replace('\\', '/') for image in train_labels]
+val_paths = [os.path.join(train_f, image[0]).replace('\\', '/') for image in val_labels]
+
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(224),
+    transforms.ToTensor()])
 
 
-torch.cuda.is_available()
-var = torch.version
+class Dataset(data.Dataset):
 
-print(torch.cuda.get_device_name(0))
-print(torch.cuda.is_available())
-print(var)
+    def __init__(self, paths, labels, transform=None):
+        self.paths = paths
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image = dcmread(f'{self.paths[index]}.dcm')
+        image = image.pixel_array
+        image = image / 255.0
+
+        image = (255 * image).clip(0, 255).astype(np.uint8)
+        image = Image.fromarray(image).convert('RGB')
+
+        label = self.labels[index][1]
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        name = self.paths[index].split("/")[-1]
+        GH = all_data['patientId'] == name
+        FIL = all_data[GH]
+        # print("From the datset loader, name", name)
+        box = [FIL['x'].values[0], FIL['y'].values[0], FIL['width'].values[0], FIL['height'].values[0]]
+
+        return image, label, box
+
+    def __len__(self):
+        return len(self.paths)
+
+train_dataset = Dataset(train_paths, train_labels, transform=transform)
+image = iter(train_dataset)
+img, label, box = next(image)
+print(label, box)
+img = np.transpose(img, (1, 2, 0))
+plt.imshow(img)
+
+img.shape
+
+train_dataset = Dataset(train_paths, train_labels, transform=transform)
+val_dataset = Dataset(val_paths, val_labels, transform=transform)
+train_loader = data.DataLoader(dataset=train_dataset, batch_size=128, shuffle=True)
+val_loader = data.DataLoader(dataset=val_dataset, batch_size=128, shuffle=False)
+
+batch = iter(train_loader)
+images, labels, _ = next(batch)
+
+image_grid = torchvision.utils.make_grid(images[:4])
+image_np = image_grid.numpy()
+img = np.transpose(image_np, (1, 2, 0))
+plt.imshow(img)
+
+model = torchvision.models.resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+model.fc = nn.Linear(num_ftrs, 2)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+num_epochs = 20
+# Train the model
+total_step = len(train_loader)
+print("before train")
+for epoch in range(num_epochs):
+     # Training step
+    print(f'epoch : {epoch}')
+    for i, (images, labels, _) in tqdm(enumerate(train_loader)):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 2000 == 0:
+            print("Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}"
+                    .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+    # Validation step
+    correct = 0
+    total = 0
+    for images, labels, _ in tqdm(val_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        predictions = model(images)
+        _, predicted = torch.max(predictions, 1)
+        total += labels.size(0)
+        correct += (labels == predicted).sum()
+    print(f'Epoch: {epoch + 1}/{num_epochs}, Val_Acc: {100 * correct / total}')
+
+model.eval()
+
+correct = 0
+total = 0
+for images, labels, _ in tqdm(val_loader):
+    images = images.to(device)
+    labels = labels.to(device)
+    predictions = model(images)
+    _, predicted = torch.max(predictions, 1)
+    total += labels.size(0)
+    correct += (labels == predicted).sum()
+print(f'Val_Acc: {100 * correct / total}')
+
+model = torch.load('C:/Users/smmm/Downloads/rsna-pneumonia-detection-challenge')
+
+model.eval()
+
+correct = 0
+total = 0
+for images, labels, _ in tqdm(val_loader):
+    images = images.to(device)
+    labels = labels.to(device)
+    predictions = model(images)
+    _, predicted = torch.max(predictions, 1)
+    total += labels.size(0)
+    correct += (labels == predicted).sum()
+print(f'Val_Acc: {100*correct/total}')
+
+
+# Load test image
+label = 0
+while(label==0):
+    pil_img, label, box = next(image)
+
+fig,ax = plt.subplots(1)
+
+Orig_img_size = 1024
+img_size = 224
+
+# 'r' means relative. 'c' means center.
+rx = ceil(box[0]*img_size/Orig_img_size)
+ry = ceil(box[1]*img_size/Orig_img_size)
+rw = ceil(box[2]*img_size/Orig_img_size)
+rh = ceil(box[3]*img_size/Orig_img_size)
+
+
+pil_img = np.transpose(pil_img, (1, 2, 0))
+print(pil_img.shape)
+rect = patches.Rectangle((rx, ry), rw, rh, linewidth=1, edgecolor='r', facecolor='none')
+ax.imshow(pil_img)
+ax.add_patch(rect)
+print("Label : ", label, box)
